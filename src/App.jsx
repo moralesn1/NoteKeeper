e624d0c7c0796728745e85771d173139922e7137
Nathaniel <nathanielmorales01@gmail.com>
2020-02-25 14:31:11 +1100

    note Outline

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
