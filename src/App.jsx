4c2823b9810a06291a66e6b6278377fb4e04daf3
Nathaniel <nathanielmorales01@gmail.com>
2020-02-28 16:27:26 +1100

    refactor note component loop in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Header from "./Header";
 import Note from "./Note";
 import notes from "./notes";
 
-function createNotes(noteItem) {
-  return <Note title={noteItem.title} content={noteItem.content} />;
-}
-
 function App() {
   return (
     <>
       <Header />
-      {notes.map(createNotes)}
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </>
   );
